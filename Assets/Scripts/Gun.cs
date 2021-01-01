da87d9b9e29718289c12a2d3061d4f41735e4589
Alexander <sashacomp2@gmail.com>
2021-01-01 17:17:49 +0300

    Added a gun script
    Добавлен скрипт стрельбы не через физику!

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private float initialForce;
+
+     
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bulletPrefab, transform.position, Quaternion.identity, transform);
+
+            var newBallBody = bulletPrefab.GetComponent<Rigidbody>();
+            
+            newBallBody.AddForce(Vector3.forward * initialForce);
+        }
+    }
+}
